f6f90c4d87ee5d279c17e3c805b9b4e2532e9442
gafmgafm <bill.martins@gmail.com>
2018-09-21 19:39:35 +1000

    Improve output indentation

--- src/project_template/custom/view_template/base_nav.view.php
+++ src/project_template/custom/view_template/base_nav.view.php
@@ -2,9 +2,9 @@
 
 use Nyht\Generator\Schema;
 
-echo '<ul class="nav flex-column">';
+echo '<ul class="nav flex-column">'.PHP_EOL;
 $keys = array_keys($schema);
 foreach ($keys as $table) {
-    echo '<li class="nav-item"><a href="/'.$schema[$table][Schema::SANE_NAME].'/" class="nav-link">'.$schema[$table][Schema::SANE_NAME].'</a></li>';
+    echo '                <li class="nav-item"><a href="/'.$schema[$table][Schema::SANE_NAME].'/" class="nav-link">'.$schema[$table][Schema::SANE_NAME].'</a></li>'.PHP_EOL;
 }
-echo '</ul>';
+echo '            </ul>'.PHP_EOL;
